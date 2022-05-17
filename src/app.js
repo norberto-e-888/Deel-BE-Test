e97f073b882423ecc5bdb092475e9d58182556fb
Norberto Cáceres <norberto.e.888@gmail.com>
2022-05-17 14:42:03 -0500

    Respond with a 403 on GET /contracts/:id in case the user is neither the client or contractor

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { sequelize } = require("./model");
+const { getProfile } = require("./middleware/getProfile");
+const app = express();
+app.use(bodyParser.json());
+app.set("sequelize", sequelize);
+app.set("models", sequelize.models);
+
+/**
+ * FIX ME!
+ * @returns contract by id
+ */
+app.get("/contracts/:id", getProfile, async (req, res) => {
+  const { Contract } = req.app.get("models");
+  const { id } = req.params;
+  const contract = await Contract.findOne({ where: { id } });
+
+  if (!contract) return res.status(404).end();
+
+  if (
+    contract.ContractorId !== req.profile.id &&
+    contract.ClientId !== req.profile.id
+  )
+    return res.status(403).end();
+
+  res.json(contract);
+});
+
+app.get("/contracts", async (req, res) => {
+  const { Contract } = req.app.get("models");
+  const contracts = await Contract.findAll({
+    where: {},
+  });
+
+  res.json(contracts);
+});
+
+module.exports = app;
