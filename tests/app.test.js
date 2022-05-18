58c0fe380bd63217b695f6e4cfbf62a984a472c9
Norberto Cáceres <norberto.e.888@gmail.com>
2022-05-18 15:20:56 -0500

    add sample e2e test using supertest and jest

---
+++ tests/app.test.js
@@ -0,0 +1,95 @@
+const app = require("../src/app");
+const request = require("supertest");
+const { faker } = require("@faker-js/faker");
+const { sequelize } = require("../src/model");
+
+describe("Deel Test API", () => {
+  let profileFactory;
+  let contractFactory;
+
+  beforeAll(async () => {
+    profileFactory = ProfileFactory(sequelize.model("Profile"));
+    contractFactory = ContractFactory(sequelize.model("Contract"));
+  });
+
+  beforeEach(async () => {
+    const Profile = sequelize.model("Profile");
+    const Contract = sequelize.model("Contract");
+    const Job = sequelize.model("Job");
+
+    await Promise.all([
+      Profile.truncate(),
+      Contract.truncate(),
+      Job.truncate(),
+    ]);
+  });
+
+  describe("GET /contracts", () => {
+    it("Rejects unauthenticated requests", async () => {
+      await request(app).get("/contracts").expect(401);
+    });
+
+    it("Returns the contracts belogning to a user as either client or contractor", async () => {
+      await profileFactory.add({ id: 10, type: "client" });
+      await profileFactory.add({ id: 11, type: "contractor" });
+      await contractFactory.add({ id: 20, ClientId: 10, ContractorId: 11 });
+
+      const clientResponse = await request(app)
+        .get("/contracts")
+        .set("profile_id", "10")
+        .expect(200);
+
+      expect(clientResponse.body.length).toBe(1);
+      expect(clientResponse.body[0].ClientId).toBe(10);
+
+      const contractorResponse = await request(app)
+        .get("/contracts")
+        .set("profile_id", "11")
+        .expect(200);
+
+      expect(contractorResponse.body.length).toBe(1);
+      expect(contractorResponse.body[0].ContractorId).toBe(11);
+    });
+  });
+});
+
+function ProfileFactory(profileModel) {
+  return {
+    add: async ({
+      id,
+      type,
+      firstName = faker.name.firstName(),
+      lastName = faker.name.lastName(),
+      profession = faker.commerce.department(),
+      balance = 0,
+    }) =>
+      profileModel.create({
+        id,
+        type,
+        firstName,
+        lastName,
+        profession,
+        balance,
+      }),
+  };
+}
+
+function ContractFactory(contractModel) {
+  return {
+    add: async ({
+      id,
+      ClientId,
+      ContractorId,
+      status = "in_progress",
+      terms = faker.lorem.sentences(2),
+    }) => {
+      contractModel.create({
+        id,
+        ClientId,
+        ContractorId,
+        terms,
+        status,
+      });
+    },
+  };
+}
